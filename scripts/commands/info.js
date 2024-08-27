04b489d14dc1dfcf52a9652b338b65a39b252180
Minicat22 <skshakil22x@gmail.com>
2024-08-27 13:07:09 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -54,6 +54,6 @@ var callback = () => api.sendMessage({body:`
 💕
 🌷🌷𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/GOLAPI.NUNUR.ODHIKARI`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
